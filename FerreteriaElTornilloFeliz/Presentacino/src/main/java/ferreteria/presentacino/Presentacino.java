120bfccbb0486b5ac3e187ce0f8a62da7ef13669
Deibi <121470260+deiviLedesma@users.noreply.github.com>
2025-05-28 21:28:24 -0700

    Add files via upload

---
+++ FerreteriaElTornilloFeliz/Presentacino/src/main/java/ferreteria/presentacino/Presentacino.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ferreteria.presentacino;
+
+/**
+ *
+ * @author SDavidLedesma
+ */
+public class Presentacino {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
